e373deb92744805731b39886d3d53e2ff6c7258c
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-02-17 16:08:35 -0800

    Stack and Node class implementation

---
+++ Data_structures/Stacks/stack.js
@@ -0,0 +1,68 @@
+// ============================================================================
+// Implementation Exercise: Stack
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement a Stack and all of its methods below!
+//
+// ------------
+// Constraints:
+// ------------
+//
+// Make sure the time and space complexity of each is equivalent to those 
+// in the table provided in the Time and Space Complexity Analysis section
+// of your Stack reading!
+//
+// -----------
+// Let's Code!
+// -----------
+
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.top) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            let temp = this.top;
+            this.top = newNode;
+            this.top.next = temp;
+        }
+        this.length++;
+        return this.length;
+    }
+
+    pop() {
+        if (!this.top) return null;
+        if (this.top === this.bootm) {
+            this.bottom = null;
+        }
+        let topNode = this.top;
+        this.top = this.top.next;
+        this.length--;
+        return topNode.value;
+    }
+
+    size() {
+        return this.length;
+    }
+}
+
+exports.Node = Node;
+exports.Stack = Stack;
