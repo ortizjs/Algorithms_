a311972349cba4ac104afeaef74f36a2c07c0cdc
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-02-24 15:07:11 -0800

    Stack and Queues Interview questions

--- Binary_search/binary_search.js
+++ Binary_search/binary_search.js
@@ -1,5 +1,13 @@
 function binarySearch(array, target) {
-
+    if (!array.length) return false;
+    let midIdx = Math.floor(array.length / 2);
+    if (target < array[midIdx]) {
+        return binarySearch(array.slice(0, midIdx), target);
+    } else if (target > array[midIdx]) {
+        return binarySearch(array.slice(midIdx + 1), target);
+    } else {
+        return true;
+    }
 }
 
 function binarySearchIndex(array, target) {
--- Data_structures/StacksAndQueuesInterviewQuestions/3_stack_queue.js
+++ Data_structures/StacksAndQueuesInterviewQuestions/3_stack_queue.js
@@ -26,17 +26,96 @@
 
 class Node {
     // TODO: Implement the Node class!
-
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
 }
 
 class Stack {
     // TODO: Implement the Stack class!
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    push(newNode) {
+        if (newNode.next) {
+            newNode.next = null;
+        }
+        if (!this.top) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const temp = this.top;
+            this.top = newNode;
+            this.top.next = temp;
+        }
+        return ++this.length;
+    }
+
+    pop() {
+        if (!this.top) return null;
+        const topNode = this.top;
+        if (this.top === this.bottom) {
+            this.bottom = null;
+        }
+        this.top = this.top.next;
+        this.length--;
+        return topNode;
+    }
+
+    size() {
+        return this.length;
+    }
 
 }
 
 class StackQueue {
     // TODO: Implement the StackQueue class!
+    constructor() {
+        this.front = null;
+        this.back = null;
+        this.inStack = new Stack();
+        this.outStack = new Stack();
+    }
 
+    enqueue(val) {
+        let newNode = new Node(val);
+        if (!this.front) {
+            this.front = newNode;
+            this.back = newNode;
+        } else {
+            this.back.next = newNode;
+            this.back = newNode;
+        }
+        this.inStack.push(new Node(newNode.value));
+        return this.size();
+    }
+
+
+    dequeue() {
+        if (!this.front) {
+            return null;
+        } else if (this.size() === 1) {
+            this.front = null;
+            this.back = null;
+        } else {
+            this.front = this.front.next;
+        }
+        if (this.outStack.size() === 0) {
+            while (this.inStack.size() > 0) {
+                this.outStack.push(this.inStack.pop());
+            }
+        }
+        let x = this.outStack.pop();
+        return x;
+    }
+
+    size() {
+        return this.inStack.size() + this.outStack.size();
+    }
 };
 
 exports.Node = Node;
