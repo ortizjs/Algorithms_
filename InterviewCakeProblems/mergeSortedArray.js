7569727f8d82f60716e50c16718c2fb4e3b728e9
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-08-28 15:58:52 -0700

    Worked on merge sorted arrays from Interview Cake JS implementation

--- InterviewCakeProblems/mergeSortedArray.js
+++ InterviewCakeProblems/mergeSortedArray.js
@@ -1,26 +1,55 @@
 function mergeSortedArrays(myList, aliceList){
-    let mergedArrays = []
-    let myCurrentIdx = 0
-    let aliceCurrentIdx = 0
-    let mergedIdx = 0
+    let mergeSortedArrays = []
+
+    let m = 0;
+    let a = 0;
     
-    while (mergedIdx < (myList.length + aliceList.length)){
-        let myCurrNum = myList[myCurrentIdx]
-        let aliceCurrNum = aliceList[aliceCurrentIdx]
-        let myListDone = myCurrentIdx >= myList.length
-        let aliceListDone = aliceCurrentIdx >= aliceList.length
-        if ((!myListDone) && (aliceListDone || myCurrNum < aliceCurrNum)) {
-            mergedArrays.push(myCurrNum)
-            myCurrentIdx++
-        } else {
-            mergedArrays.push(aliceCurrNum)
-            aliceCurrentIdx++
+    while ((m < myList.length &&  a < aliceList.elngth) || (myList[m] != undefined || aliceList[a] != undefined)) {
+        let mySell = myList[m]; 
+        let aliceSell = aliceList[a];
+        if (mySell === undefined) { mySell = Infinity}
+        if (aliceSell === undefined) { aliceSell = Infinity}
+
+        if (mySell <= aliceSell) {
+            mergeSortedArrays.push(mySell);
+            m++;
+        } 
+        else {
+            mergeSortedArrays.push(aliceSell);
+            a++;
         }
-        mergedIdx++
     }
-    return mergedArrays
+    return mergeSortedArrays;
+}
+
+
+function mergeSortedArraysICImplementation(myArray, aliceArray) {
+    let mergedSortedArray = [];
+    let myCurrentIdx = 0;
+    let aliceCurrentIdx = 0;
+    let mergedCurrentIdx = 0;
+
+    while (mergedCurrentIdx < (myArray.length + aliceArray.length)) {
+        const isMyArrayExhausted = myCurrentIdx >= myArray.length;
+        const isAliceArrayExhausted = aliceCurrentIdx >= aliceArray.length;
+
+        if (!isMyArrayExhausted && (isAliceArrayExhausted || myArray[myCurrentIdx] < aliceArray[aliceCurrentIdx])) {
+            mergedSortedArray[mergedCurrentIdx] = myArray[myCurrentIdx];
+            myCurrentIdx++;
+        } else {
+            mergedSortedArray[mergedCurrentIdx] = aliceArray[aliceCurrentIdx];
+            aliceCurrentIdx++;
+        }
+        mergedCurrentIdx++;
+    }
+    return mergedSortedArray
 }
 my_list = [3, 4, 6, 10, 11, 15, 20, 21]
 alices_list = [1, 5, 8, 12, 14, 19]
+const myArray = [3, 4, 6, 10, 11, 15];
+const alicesArray = [1, 5, 8, 12, 14, 19];
 
 console.log(mergeSortedArrays(my_list, alices_list))
+console.log(mergeSortedArrays(myArray, alicesArray))
+console.log(mergeSortedArraysICImplementation(my_list, alices_list))
+console.log(mergeSortedArraysICImplementation(myArray, alicesArray))
