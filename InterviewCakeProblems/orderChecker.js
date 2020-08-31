25ed6dfbba9a96190f5fac158611874ebe974145
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-08-31 15:38:39 -0700

    Worked on orderChecker problem from InterviewCake both iteratively and recursive solution

--- InterviewCakeProblems/orderChecker.js
+++ InterviewCakeProblems/orderChecker.js
@@ -1,42 +1,49 @@
 function orderChecker(takeOutOrders, dineInOrders, servedOrders){
-    var takeOutIdx = 0
-    var dineInIdx = 0
-    for (let i = 0; i < servedOrders.length; i++) {
-        let order = servedOrders[i]
-        if (takeOutOrders.indexOf(order) != -1) {
-            if (takeOutIdx == takeOutOrders.indexOf(order)) {
-                takeOutIdx += 1
-            } else {
-                return false
-            }
+    let takeOutCurrentIdx = 0;
+    let dineInCurrentIdx = 0;
+    let servedCurrentIdx = 0;
+
+    while (servedCurrentIdx < servedOrders.length) {
+        let currentServedOrder = servedOrders[servedCurrentIdx];
+
+        if (dineInCurrentIdx <= dineInOrders.length - 1 && (currentServedOrder === dineInOrders[dineInCurrentIdx])) {
+            dineInCurrentIdx++;
+        } else if (takeOutCurrentIdx <= takeOutOrders.length - 1 && (currentServedOrder == takeOutOrders[takeOutCurrentIdx])) {
+            takeOutCurrentIdx++;
         } else {
-            if (dineInIdx == dineInOrders.indexOf(order)) {
-                dineInIdx += 1
-            } else {
-                return false
-            }
+            return false;
         }
-    };
-    return true
+        servedCurrentIdx++;
+    }
+
+    if (dineInCurrentIdx != dineInOrders.length || takeOutCurrentIdx != takeOutOrders.length) {
+        return false;
+    }
+    
+    return true;
 }
 
 function orderCheckerRecur(takeOutOrders, dineInOrders, servedOrders, takeOutIdx=0, dineInIdx=0, servedIdx=0) {
-    if (servedIdx === servedOrders.length) {
-        return true
-    } 
-    if ((takeOutIdx < takeOutOrders.length) && takeOutOrders[takeOutIdx] == servedOrders[servedIdx]) {
-        takeOutIdx++
-    } else if ((dineInIdx < dineInOrders.length) && dineInOrders[dineInIdx] == servedOrders[servedIdx]) {
+    if (servedIdx === servedOrders.length - 1) {
+        return true;
+    }
+    if (servedOrders[servedIdx] === takeOutOrders[takeOutIdx]) {
+        takeOutIdx++;
+    } else if (servedOrders[servedIdx] === dineInOrders[dineInIdx]) {
         dineInIdx++
     } else {
-        return false
+        return false;
     }
-    servedIdx++
-    return orderCheckerRecur(takeOutOrders,dineInOrders,servedOrders,takeOutIdx, dineInIdx, servedIdx)
+    servedIdx++;
+    return orderCheckerRecur(takeOutOrders, dineInOrders, servedOrders, takeOutIdx, dineInIdx, servedIdx);
 }
 
 console.log(orderChecker([1, 3, 5], [2, 4, 6], [1, 2, 4, 6, 5, 3]))
+console.log(orderChecker([1, 3, 5, 8, 10], [2, 4, 6], [1, 2, 4, 6, 5, 3, 10, 8]))
 console.log(orderChecker([17, 8, 24], [12, 19, 2], [17, 8, 12, 19, 24, 2]))
+console.log(orderChecker([17, 8, 24], [12, 19, 2,3], [17, 8, 12, 19, 24, 2,3]))
 console.log("-------------- recursion version below!")
 console.log(orderCheckerRecur([1, 3, 5], [2, 4, 6], [1, 2, 4, 6, 5, 3]))
+console.log(orderCheckerRecur([1, 3, 5, 8, 10], [2, 4, 6], [1, 2, 4, 6, 5, 3, 10, 8]))
 console.log(orderCheckerRecur([17, 8, 24], [12, 19, 2], [17, 8, 12, 19, 24, 2]))
+console.log(orderCheckerRecur([17, 8, 24], [12, 19, 2, 3], [17, 8, 12, 19, 24, 2, 3]))
