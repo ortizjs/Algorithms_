5322cc776720766a3ee0564eef5d87dfab19c4d7
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-09-01 15:59:55 -0700

    Redo of inFlightEntertainment from InterviewCake js implementation

--- InterviewCakeProblems/inFlightEntertainment.js
+++ InterviewCakeProblems/inFlightEntertainment.js
@@ -1,13 +1,17 @@
 function inFlightEntertainment(flight_length, movie_lengths) {
-    let movies = new Set();
-    let first_movie;
-    for (let i = 0; i < movie_lengths.length; i++){
-        first_movie = movie_lengths[i];
-        let second_movie = flight_length - first_movie;
-        if (movies.has(second_movie)) {
-            return true;
-        }
-        movies.add(first_movie)
-    }
-    return false;
+   let seenMovies = new Set();
+   
+   for (let i = 0; i < movie_lengths.length; i++) {
+       let firstMovie = movie_lengths[i];
+       let secondMovie = flight_length - firstMovie;
+       if (seenMovies.has(secondMovie)) {
+           return true;
+       } else {
+           seenMovies.add(firstMovie)
+       }
+   }
+   return false;
 }
+
+console.log(inFlightEntertainment(100, [25, 30, 60, 75, 90, 80, 40, 70]))
+console.log(inFlightEntertainment(100, [25, 30, 60, 85, 90, 80, 50]))
