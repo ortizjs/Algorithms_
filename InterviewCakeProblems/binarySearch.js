adad7dbc025a15044d08fa3badd94b84bb632c44
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-09-17 12:11:16 -0700

    Redo binarySearch recur version Js

--- InterviewCakeProblems/binarySearch.js
+++ InterviewCakeProblems/binarySearch.js
@@ -19,10 +19,29 @@ function binarySearch(arr, target) {
         }
     }
     return false;
+};
+
+
+function binarySearchRecur(nums, target) {
+    if (nums.length < 1) {
+        return false;
+    }
+    let midIdx = Math.min(nums.length / 2);
+    if (target < nums[midIdx]) {
+        return binarySearchRecur(nums.slice(0, midIdx), target);
+    } else if (target > nums[midIdx]) {
+        return binarySearchRecur(nums.slice(midIdx + 1), target);
+    } else {
+        return true;
+    }
 }
 
 
 console.log("------ Iterative version of Binary Search")
+console.log(binarySearch([1,2,3,4,5,6,7], 7));
+console.log(binarySearch([0, 10, 21, 33, 44, 55, 88, 91, 100], 21));
+console.log(binarySearch([11, 22, 33, 44, 55, 67, 77], 66));
+console.log("------ Recursive version of Binary Search")
 console.log(binarySearch([1,2,3,4,5,6,7], 7));
 console.log(binarySearch([0, 10, 21, 33, 44, 55, 88, 91, 100], 21));
 console.log(binarySearch([11, 22, 33, 44, 55, 67, 77], 66));
