a37eebe3725375da8cb361635f87beb3197a7f0a
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-09-28 22:51:06 -0700

    Redo of BinarySearchTreeChecker from InterviewCake JS their solution as mine fails... will come back

--- InterviewCakeProblems/binaryTreeChecker.js
+++ InterviewCakeProblems/binaryTreeChecker.js
@@ -1,35 +1,64 @@
-// const binaryTreeNode = require("./binaryTreeNode");
-
-// const binaryTreeNode = require("./binaryTreeNode");
 binaryTreeNode = require("./binaryTreeNode");
 
+// function binarySearchTreeChecker(root){
+//     if (!root) {
+//         return true;
+//     }
+//     let rootNodeValue = root.value;
 
-// binaryTreeNode = require(".binaryTreeNode");
+//     let stack = [root];
+//     while (stack.length) {
+        
+//         let node = stack.shift();
 
-function binarySearchTreeChecker(root){
+//         if (node.right) {
+//             stack.push(node.right);
+//             if (node.value > node.right.value || (node.value != rootNodeValue && node.left.value > rootNodeValue)) {
+//                 return false;
+//             }
+//         }
+
+//         if (node.left) {
+//             stack.push(node.left);
+//             if (node.left.value > node.value || (node.value != rootNodeValue && node.right.value < rootNodeValue)) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
+
+function binarySearchTreeChecker(root) {
+   let nodeAndBounds = [{node: root, lowerBound: -Infinity, upperBound: Infinity}];
+
+   while (nodeAndBounds.length) {
+       let {node, lowerBound, upperBound} = nodeAndBounds.pop();
+
+       if (node.value <= lowerBound || node.value >= upperBound) {
+           return false;
+       }
+       if (node.right) {
+           nodeAndBounds.push({node: node.right, lowerBound: node.value, upperBound});
+       }
+       if (node.left) {
+           nodeAndBounds.push({node: node.left, lowerBound, upperBound: node.value});
+       }
+   }
+   return true;
+}
+
+function binarySearchTreeCheckerRecur(root, lowerBound, upperBound) {
     if (!root) {
         return true;
     }
+    if (lowerBound === undefined ? -Infinity : lowerBound);
+    if (upperBound === undefined ? Infinity : upperBound);
 
-    let stack = [root];
-    while (stack.length) {
-        
-        let node = stack.pop();
-
-        if (node.right) {
-            stack.push(node.right);
-            if (node.value > node.right.value) {
-                return false;
-            }
-        }
-        if (node.left) {
-            stack.push(node.left);
-            if (node.left.value > node.value) {
-                return false;
-            }
-        }
+    if (root.value <= lowerBound || root.value >= upperBound) {
+        return false;
     }
-    return true;
+    return (binarySearchTreeCheckerRecur(root.right, root.value, upperBound) &&
+            binarySearchTreeCheckerRecur(root.left, lowerBound, root.value));
 }
 
 let a = new binaryTreeNode(50);
@@ -56,6 +85,7 @@ d.right = j;
 e.left = k;
 
 console.log(binarySearchTreeChecker(a));
+console.log(binarySearchTreeCheckerRecur(a));
 
 let aa = new binaryTreeNode(50);
 let bb = new binaryTreeNode(17);
@@ -81,3 +111,22 @@ dd.right = jj;
 ee.left = kk;
 
 console.log(binarySearchTreeChecker(aa));
+console.log(binarySearchTreeCheckerRecur(aa));
+
+let aaa = new binaryTreeNode(50);
+let bbb = new binaryTreeNode(30);
+let ccc = new binaryTreeNode(80);
+let ddd = new binaryTreeNode(20);
+let eee = new binaryTreeNode(60);
+let fff = new binaryTreeNode(70);
+let ggg = new binaryTreeNode(90);
+
+aaa.left = bbb;
+aaa.right = ccc;
+bbb.left = ddd;
+bbb.right = eee;
+ccc.left = fff;
+ccc.right = ggg;
+
+console.log(binarySearchTreeChecker(aaa));
+console.log(binarySearchTreeCheckerRecur(aaa));
