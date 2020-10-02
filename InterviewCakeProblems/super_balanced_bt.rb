ae0e970e86af338d8d600e089258b3ef01b00b55
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-10-01 21:57:30 -0700

    Redo of super balanced binary tree from InterviewCake Ruby

---
+++ InterviewCakeProblems/super_balanced_bt.rb
@@ -0,0 +1,57 @@
+class BinaryTreeNode
+    attr_accessor :value, :left, :right
+    
+    def initialize(value)
+        @value = value
+        @left = nil
+        @right = nil
+    end
+end
+
+def super_balanced(root)
+    if !root
+        return true
+    end
+
+    stack = [[root, 0]]
+    depths = []
+
+    while stack.length > 0
+        node_pair = stack.pop
+        node = node_pair[0]
+        depth = node_pair[1]
+
+        if root.left == nil && root.right == nil
+            if depths.index(depth) == nil
+                depths << depth 
+                if depths.length > 2 || depths.length == 2 && (depths[0] - depths[1]).abs > 1
+                    return false
+                end
+            end
+        else
+            if node.left
+                stack. << [node.left, depth + 1]
+            end
+
+            if node.right
+                stack << [node.right, depth + 1]
+            end
+        end
+    end
+    true
+end
+
+a = BinaryTreeNode.new('a');
+b = BinaryTreeNode.new('b');
+c = BinaryTreeNode.new('c');
+d = BinaryTreeNode.new('d');
+e = BinaryTreeNode.new('e');
+f = BinaryTreeNode.new('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+puts super_balanced(a);
