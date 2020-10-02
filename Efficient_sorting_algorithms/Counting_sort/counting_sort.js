f2d48faa0acdaac9d8e72be3928ef92472443922
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-10-02 09:31:25 -0700

    Counting sort JS

--- Efficient_sorting_algorithms/Counting_sort/counting_sort.js
+++ Efficient_sorting_algorithms/Counting_sort/counting_sort.js
@@ -1,7 +1,23 @@
 function countingSort(arr, max) {
+    let countingArray = new Array(max + 1).fill(0);
+    let finalArray = [];
 
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        countingArray[num]++
+    }
+
+    for (let j = 0; j < arr.length + 1; j++) {
+        let counter = countingArray[j];
+        while (counter > 0) {
+            finalArray.push(j);
+            counter--;
+        }
+    }
+    return finalArray
 }
 
+console.log(countingSort([4, 3, 5, 6, 7, 8, 3, 1], 8))
 
 module.exports = {
     countingSort
