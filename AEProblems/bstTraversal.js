b19e601a1f84b9f2add2a86a55552b1567495377
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-12-07 14:59:00 -0800

    Completed AE BST Traversal JS

---
+++ AEProblems/bstTraversal.js
@@ -0,0 +1,28 @@
+function inOrderTraverse(tree, array) {
+    // Write your code here.
+    if (!tree) return;
+    inOrderTraverse(tree.left, array);
+    array.push(tree.value);
+    inOrderTraverse(tree.right, array);
+    return array
+}
+
+function preOrderTraverse(tree, array) {
+    // Write your code here.
+    if (!tree) return;
+    array.push(tree.value);
+    preOrderTraverse(tree.left, array);
+    preOrderTraverse(tree.right, array);
+    return array
+}
+
+function postOrderTraverse(tree, array) {
+    // Write your code here.
+    if (!tree) return;
+    postOrderTraverse(tree.left, array);
+    postOrderTraverse(tree.right, array);
+    array.push(tree.value)
+    return array;
+}
+
+
