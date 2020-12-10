13b8345a0b135b0d5db5786a5841a6accdbd65ed
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-12-09 19:54:33 -0800

    Completed Leetcode's Daily Challange Dec 9th Binary Search Tree Iterator Python

---
+++ LeetcodeQuestions/leetcode_challenges/december_challange/BSTIterator.py
@@ -0,0 +1,21 @@
+class BSTIterator:
+
+    def __init__(self, root):
+        self.index = -1
+        self.sorted_nodes = []
+        self.inOrder(root)
+
+    def next(self):
+        self.index += 1
+        if self.index < len(self.sorted_nodes):
+            return self.sorted_nodes[self.index]
+
+    def hasNext(self):
+        return self.index < len(self.sorted_nodes)
+
+    def inOrder(self, root):
+        if not root:
+            return
+        self.inOrder(root.left)
+        self.sorted_nodes.append(root.val)
+        self.inOrder(root.right)
