139a4d0736a0ed7ba5732122daafe2cd5c8f444f
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-12-22 10:40:53 -0800

    Rewrote a little previous answer to smallest range II Python

--- LeetcodeQuestions/leetcode_challenges/december_challange/smallest_range_II.py
+++ LeetcodeQuestions/leetcode_challenges/december_challange/smallest_range_II.py
@@ -1,14 +1,16 @@
-class Solution:
-    def smallestRangeII(self, A: List[int], K: int) -> int:
-        A = sorted(A)
-        min_val = A[0]
-        max_val = A[-1]
-        current = max_val - min_val
-        for i in range(len(A) - 1):
-            a = A[i]
-            b = A[i+1]
-            val_one = max_val - K
-            val_two = min_val + K
+def smallestRangeII(A, K):
+    A = sorted(A)
+    min_val = A[0]
+    max_val = A[-1]
+    current = max_val - min_val
+    for i in range(len(A) - 1):
+        a = A[i]
+        b = A[i+1]
+        val_one = max_val - K
+        val_two = min_val + K
 
-            current = min(current, max(val_one, a + K) - min(val_two, b - K))
-        return current
+        current = min(current, max(val_one, a + K) - min(val_two, b - K))
+    return current
+
+
+print(smallestRangeII([4, 2, 3, 4, 5, 6, 4], 3))
