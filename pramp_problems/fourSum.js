ffdb4d7677d044121043c8930c680f4b6fc45a0e
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-12-29 11:23:18 -0800

    Added secondary solution to fourSum problem JS

--- pramp_problems/fourSum.js
+++ pramp_problems/fourSum.js
@@ -29,6 +29,36 @@ function twoSum(array, idx, target) {
     return [];
 }
 
+function fourSumTwo(array, s) {
+    let pairs_hash = {}
+    let results = []
+    let sorted_nums = arr.sort((a, b) => {
+        return a - b;
+    });
+
+    for (let i = 1; i < sorted_nums.length - 1; i++) {
+        for (let j = i + 1; j < sorted_nums.length; j++) {
+            let first_sum = sorted_nums[i] + sorted_nums[j];
+            let diff = s - first_sum
+            if (diff in pairs_hash) {
+                for (let pair of pairs_hash[diff]) {
+                    results.push(pair.concat([sorted_nums[i], sorted_nums[j]]))
+                }
+            }
+        }
+
+        for (let k = 0; k < i; k++) {
+            let second_sum = sorted_nums[i] + sorted_nums[k]
+            if (second_sum in pairs_hash) {
+                pairs_hash[second_sum].push([sorted_nums[k], sorted_nums[i]])
+            } else {
+                pairs_hash[second_sum] = [[sorted_nums[k], sorted_nums[i]]]
+            }
+        }
+    }
+    return results
+}
+
 let arr = [2, 7, 4, 0, 9, 5, 1, 3]
 s = 20
 
@@ -42,3 +72,4 @@ hash = {"11":[[7,4]],
 */
 
 console.log(fourSum(arr, s))
+console.log(fourSumTwo(arr, s))
