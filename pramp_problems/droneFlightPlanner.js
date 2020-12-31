91fa7ae26078d658eb52497dccd67f276908d02c
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-12-30 16:22:58 -0800

    Refactored drone flight planner JS

--- pramp_problems/droneFlightPlanner.js
+++ pramp_problems/droneFlightPlanner.js
@@ -3,30 +3,26 @@ function calcDroneMinEnergy(route) {
     let minEnergy = 0
     let netEnergy = 0
 
-    for (let i=0
-         i < route.length
-         i++) {
-
+    for (let i = 0; i < route.length; i++) {
         if (i === route.length - 1) {
             return minEnergy
         }
+        let currentAlt = route[i][2]
+        let nextAlt = route[i + 1][2];
+        let distance = nextAlt - currentAlt;
 
-        let currentPoint = route[i]
-        let currAlt = currentPoint[2]
-
-        let nextPoint = route[i + 1]
-        let nextAlt = nextPoint[2]
-
-        let disToTravel = nextAlt - currAlt
-        if (disToTravel > netEnergy) {
-            return Math.abs(disToTravel - netEnergy)
-        } else if (nextAlt < currAlt) {
-            netEnergy += (currAlt - nextAlt)
-        } else if (nextAlt > currAlt) {
-            netEnergy -= nextAlt
-        } else {
+        if (distance > netEnergy) {
+            return Math.abs(netEnergy - distance);
+        } else if (currentAlt > nextAlt) {
+            netEnergy += Math.abs(distance);
+        } else if (currentAlt < nextAlt) {
+            netEnergy -= Math.abs(distance);
             minEnergy = netEnergy
         }
     }
-    return Math.abs(minEnergy)
+    return minEnergy
+
 }
+
+
+console.log(calcDroneMinEnergy([[0, 2, 10], [3, 5, 0], [9, 20, 6], [10, 12, 15], [10, 10, 8]]))
