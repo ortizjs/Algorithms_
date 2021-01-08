bce48f12be7ed54492a33f15b6d15e65d591d9c6
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-01-08 10:37:26 -0800

    freeCodeCamp's DP howSum problem JS

---
+++ Memoization/howSum.js
@@ -0,0 +1,22 @@
+function howSum(targetSum, numbers, memo = {}) {
+    if (targetSum in memo) return memo[targetSum];
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+
+    for (let num of numbers) {
+        let diff = targetSum - num;
+        let diffResult = howSum(diff, numbers, memo)
+        if (diffResult !== null) {
+            memo[targetSum] = [...diffResult, num]
+            return [...diffResult, num]
+        }
+    }
+    memo[targetSum] = null;
+    return null;
+}
+
+console.log(howSum(7, [2, 3]))
+console.log(howSum(7, [5, 3, 4, 7]))
+console.log(howSum(7, [2, 4]))
+console.log(howSum(8, [2, 3, 5]))
+console.log(howSum(300, [7, 14]))
