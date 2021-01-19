88365aa11799ef2caaccc8b307fe06e12253f5a7
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-01-18 21:24:37 -0800

    Leetcode's daily challenge Jan 18 Max Numberof K-Sum Pairs JS

---
+++ LeetcodeQuestions/leetcode_challenges/January_challange/maxNumberofK-SumPairs.js
@@ -0,0 +1,38 @@
+var maxOperations = function (nums, k, memo = {}) {
+    let sortedNums = nums.sort((a, b) => { return a - b });
+    let start = 0;
+    let end = nums.length - 1;
+    let counter = 0;
+
+    while (start < end) {
+        let sum = nums[start] + nums[end];
+        if (sum < k) {
+            start++;
+        } else if (sum > k) {
+            end--;
+        } else {
+            end--;
+            start++;
+            counter++;
+        }
+    }
+    return counter;
+    //     if (k in memo) return memo[k];
+    //     if (k === 0) return 1;
+    //     // if (k < 0) return -1;
+    //     let counter = 0;
+
+    //     for (let i = 0; i < nums.length; i++) {
+    //         let diff = k - nums[i];
+    //         // let remainderNums = [...nums.slice(0,i), ...nums.slice(i + 1, nums.length)]
+    //         let remainderNums = nums.slice(i + 1, nums.length)
+    //         // console.log(remainderNums)
+    //         let remainders = maxOperations(remainderNums, diff, memo)
+    //         if (remainders !== -1) {
+    //             counter += remainders
+    //             memo[k] = counter
+    //         }
+    //     }
+    //     memo[k] = counter;
+    //     return counter
+}
