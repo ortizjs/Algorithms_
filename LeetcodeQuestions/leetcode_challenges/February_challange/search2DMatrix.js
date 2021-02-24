7b617a0448d94d5bda441b45d5659fcb8ce60270
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-02-23 18:13:24 -0600

    Leetcode daily challange Search 2D Matrix JS

---
+++ LeetcodeQuestions/leetcode_challenges/February_challange/search2DMatrix.js
@@ -0,0 +1,21 @@
+var searchMatrix = function (matrix, target) {
+    let i = 0;
+    let j = matrix[0].length - 1;
+
+    while (i < matrix.length && j >= 0) {
+        if (matrix[i][j] === target) {
+            return true;
+        } else if (matrix[i][j] > target) {
+            j--;
+        } else {
+            i++;
+        }
+    }
+    return false;
+};
+
+console.log(searchMatrix([[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 5))
+
+console.log(searchMatrix([[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 20))
+
+console.log(searchMatrix([[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 25))
