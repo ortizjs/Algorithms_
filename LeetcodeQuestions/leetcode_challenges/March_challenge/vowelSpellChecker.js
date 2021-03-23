45533437688f3a305f0cc7c757e4df6ed5b69164
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-03-22 17:06:57 -0700

    Leetcode Daily challange Vowel SpellChecker JS

---
+++ LeetcodeQuestions/leetcode_challenges/March_challenge/vowelSpellChecker.js
@@ -0,0 +1,50 @@
+function devowel(word) {
+    let newWord = ""
+    let vowels = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"])
+    // console.log(vowels)
+    for (let char of word) {
+        if (vowels.has(char)) {
+            newWord += "*"
+        } else {
+            newWord += char
+        }
+    }
+    return newWord;
+}
+
+var spellchecker = function (wordlist, queries) {
+    let words = [];
+    let wordSet = new Set([]);
+    let caseIn = new Map();
+    let consonants = new Map();
+
+    for (let word of wordlist) {
+        let lowerCaseWord = word.toLowerCase()
+        wordSet.add(word)
+        if (!caseIn.has(lowerCaseWord)) {
+            caseIn.set(lowerCaseWord, word);
+        }
+        if (!consonants.has(devowel(lowerCaseWord))) {
+            consonants.set(devowel(lowerCaseWord), word);
+        }
+    }
+
+    for (let word of queries) {
+        let lowerCaseWord = word.toLowerCase()
+        if (wordSet.has(word)) {
+            words.push(word)
+        } else if (caseIn.has(lowerCaseWord)) {
+            words.push(caseIn.get(lowerCaseWord))
+        } else if (consonants.has(devowel(lowerCaseWord))) {
+            words.push(consonants.get(devowel(lowerCaseWord)));
+        } else {
+            words.push("")
+        }
+    }
+    console.log(consonants)
+    return words;
+};
+
+let wordlist = ["KiTe", "kite", "hare", "Hare"]
+let queries = ["kite", "Kite", "KiTe", "Hare", "HARE", "Hear", "hear", "keti", "keet", "keto"]
+console.log(spellchecker(wordlist, queries))
