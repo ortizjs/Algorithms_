b7554d98ae5012579ad87de5c107c801378bee29
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-03-24 16:43:21 -0700

    Leetcode daily challange Advantage Shuffle JS

---
+++ LeetcodeQuestions/leetcode_challenges/March_challenge/advantage_shuffle.js
@@ -0,0 +1,24 @@
+function advantageCount(A, B) {
+    let indexesOfB = B.map((e, i) => i).sort((a, b) => B[a] - B[b]);
+    let sortedA = A.sort((a, b) => a - b);
+    let answer = new Array(A.length);
+    let unused = [];
+    let i = 0;
+
+    for (let idx of indexesOfB) {
+        while (i < A.length && sortedA[i] < B[idx]) {
+            unused.push(sortedA[j]);
+            i++;
+        }
+
+        if (i === A.length) {
+            answer[idx] = unused.pop();
+        } else {
+            answer[idx] = sortedA[i];
+            i++;
+        }
+    }
+    return answer;
+}
+
+console.log(advantageCount([2, 7, 11, 15], [1, 10, 4, 11]));
