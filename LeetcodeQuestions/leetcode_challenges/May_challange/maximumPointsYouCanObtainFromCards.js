2877cc12d42c60f4ecffc2faef51792ad5b2a0e0
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-05-11 11:39:55 -0700

    Leetcode daily challange Maximum Points You Cana Obtain from Cards 1423 JS

---
+++ LeetcodeQuestions/leetcode_challenges/May_challange/maximumPointsYouCanObtainFromCards.js
@@ -0,0 +1,22 @@
+function maxScore(cardPoints, k) {
+    let n = cardPoints.length;
+    let sum = cardPoints.reduce((acc, ele) => acc + ele);
+    let total = 0;
+    let window = 0;
+
+    if (k === n) return sum;
+
+    for (let i = 0; i < n - k - 1; i++) {
+        window += cardPoints[i];
+    }
+
+    for (let i = n - k - 1; i < n; i++) {
+        window += cardPoints[i];
+        total = Math.max(total, sum - window);
+        window -= cardPoints[i - (n - k - 1)];
+    }
+    return total;
+}
+
+console.log(maxScore([1,2,3,4,5,6,1], 3))
+console.log(maxScore([10,2,3,4,5,6,1], 3))
