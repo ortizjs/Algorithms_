cfd9090ee404998ea8d9cea5cc41bd77df51c717
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-05-17 15:19:58 -0700

    Added a faster way to do permutations js

--- LeetcodeQuestions/permutations-46.js
+++ LeetcodeQuestions/permutations-46.js
@@ -2,28 +2,59 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// var permute = function(nums) {
+//     if (!nums.length || nums === null) return [];
+//     let permutations = [];
+//     let used = new Array(nums.length).fill(false);
+    
+//     generatePermutations(nums, permutations, used, []);
+//     return permutations;
+// };
+
+// function generatePermutations(nums, permutations, used, current) {
+//     if (current.length === nums.length) {
+//         permutations.push(Array.from(current));
+//         return;
+//     }
+    
+//     for (let i = 0; i < nums.length; i++) {
+//         if (used[i]) continue;
+//         used[i] = true;
+//         generatePermutations(nums, permutations, used, [...current, nums[i]]);
+//         used[i] = false;
+//     }
+// };
+
 var permute = function(nums) {
     if (!nums.length || nums === null) return [];
     let permutations = [];
-    let used = new Array(nums.length).fill(false);
+    let charMap = {};
     
-    generatePermutations(nums, permutations, used, []);
+    for (let n of nums) {
+        if (n in charMap) {
+            charMap[n]++;
+        } else {
+            charMap[n] = 1;
+        }
+    }
+    generatePermutations(nums, permutations, charMap, []);
     return permutations;
 };
 
-function generatePermutations(nums, permutations, used, current) {
+function generatePermutations(nums, permutations, charMap, current) {
     if (current.length === nums.length) {
         permutations.push(Array.from(current));
         return;
     }
     
-    for (let i = 0; i < nums.length; i++) {
-        if (used[i]) continue;
-        used[i] = true;
-        generatePermutations(nums, permutations, used, [...current, nums[i]]);
-        used[i] = false;
+    for (let num in charMap) {
+        if (charMap[num] > 0) {
+            charMap[num]--;
+            generatePermutations(nums, permutations, charMap, [...current, num])
+            charMap[num]++;
+        }
     }
-};
+}
 
 
 console.log(permute([1,2,3]))
