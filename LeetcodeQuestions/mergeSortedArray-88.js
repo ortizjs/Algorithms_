15e26b1aeca982182e5d81fa49428d2e29d743a4
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-05-19 19:51:01 -0700

    Leetcode 88 Merge Sorted Array js

---
+++ LeetcodeQuestions/mergeSortedArray-88.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    m--;
+    n--;
+    let idx = nums1.length - 1;
+    while (m >= 0 || n >= 0) {
+        if (m < 0) {
+            nums1[idx] = nums2[n];
+            n--;
+        } else if (n < 0) {
+            nums1[idx] = nums1[m];
+            m--;
+        } else {
+            if (nums1[m] <= nums2[n]) {
+                nums1[idx] = nums2[n]
+                n--;
+            } else {
+                nums1[idx] = nums1[m];
+                m--;
+            }
+        }
+        idx--;
+    }
+};
+
+console.log(merge([1,2,3,0,0,0], 1, [2,5,6], 3))
+
