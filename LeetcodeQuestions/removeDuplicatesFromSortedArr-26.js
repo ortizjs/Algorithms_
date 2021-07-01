0c1f3f09d0521ce2d389c7db7f76a17bef423f77
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-07-01 15:19:15 -0700

    Leetcode 26 Remove Duplicates js

--- LeetcodeQuestions/removeDuplicatesFromSortedArr-26.js
+++ LeetcodeQuestions/removeDuplicatesFromSortedArr-26.js
@@ -1,16 +1,20 @@
 /**
  * @param {number[]} nums
  * @return {number}
+ * Time: O(n) for the length of the input array.
+ * Space: O(1) as there is no additional data strucuted used.
  */
 var removeDuplicates = function(nums) {
-    let idx = 1;
-    for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] !== nums[i + 1]) {
-            nums[idx] = nums[i + 1];
+    let idx = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[idx] !== nums[i]) {
             idx++;
+            nums[idx] = nums[i];
         }
     }
-    return idx;
+    return idx + 1;
 };
 
 console.log(removeDuplicates([1,1,2]));
+console.log(removeDuplicates([1,1,2,3,4,5,6,7,7,7,7]));
